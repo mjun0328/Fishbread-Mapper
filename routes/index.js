5d8bd49dda794d3a66b2cb463daa8478b26a00fc
Kim Minjun <me@etimel.dev>
2024-11-23 19:48:01 +0900

    View Store's Review

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("map", { title: "우리 동네 붕어빵 - 팥붕슈붕" });
+  res.render("index", { title: "우리 동네 붕어빵 - 팥붕슈붕" });
 });
 
 module.exports = router;
