ff9b28350750e213c62046dd3f8739b12a4c36c0
Kim Minjun <me@etimel.dev>
2024-12-05 10:21:38 +0900

    Fix: Make CommonJS not ES Module

--- routes/api/middleware.js
+++ routes/api/middleware.js
@@ -1,6 +1,8 @@
-export const forceLogin = (req, res, next) => {
+const forceLogin = (req, res, next) => {
   if (!req.session.user) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   next();
 };
+
+module.exports = { forceLogin };
